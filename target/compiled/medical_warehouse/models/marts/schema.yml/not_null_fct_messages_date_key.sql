00ef3afa3e9d6cb58994c286a00bfa7ae77a8f87
Melat Tewachew <melattewachew@gmail.com>
2026-01-18 17:07:57 +0300

    merge from  medical_warehouse

---
+++ target/compiled/medical_warehouse/models/marts/schema.yml/not_null_fct_messages_date_key.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select date_key
+from "medical_warehouse"."analytics_analytics"."fct_messages"
+where date_key is null
+
+
---
+++ target/compiled/medical_warehouse/models/marts/schema.yml/not_null_fct_messages_message_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select message_id
+from "medical_warehouse"."analytics_analytics"."fct_messages"
+where message_id is null
+
+
